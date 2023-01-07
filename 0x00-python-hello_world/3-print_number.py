e0b3beb3b8ca85d70bf25705d3cd1673c967be48
joseph kouadio <Josephkouadio82@gmail.com>
2023-01-07 00:55:42 +0000

    print number

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 
 number = 98
 
 print(f"{number:d} Battery street")
-=======
-number = 98
-print("\"f"{98} Battery street")
->>>>>>> bf7090f5b1401241f64c694822436bb424813d70
